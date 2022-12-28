cdd19fd22ab7d0d88a44ff2c5350b77df896c436
Johanna Smriti <64794456+johannasmriti@users.noreply.github.com>
2022-12-28 12:59:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Landing from "./pages/Landing/Landing";
+import Call from "./pages/Call/Call";
+
+function App() {
+  return (
+    <div className="App">
+      <Call />
+    </div>
+  );
+}
+
+export default App;
